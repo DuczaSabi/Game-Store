313ef864b92f5e0ecc38480fdb35fa0d378a63c6
AndoPatrik <screadern@gmail.com>
2023-03-27 22:56:53 +0200

    UPDATE:  fetchGames endpoint support categ and serach

--- API/src/controllers/gameController.js
+++ API/src/controllers/gameController.js
@@ -1,10 +1,18 @@
 const client = require("../config/databaseConnection.js");
 async function fetchGames (req, res) {
-  let categ = req.query.category
-  if (!categ) categ = 'all'
-  const result = await client.query(`SELECT * FROM sortgenre('${ categ }')`);
-  res.status(200);
-  res.json(result.rows);
+  let { category, search } = req.query
+
+  if (search) {
+    const result = await client.query(`SELECT * FROM search('${ search }')`);
+    res.status(200);
+    res.json(result.rows);
+  }
+  else {
+    if (!category) category = 'all'
+    const result = await client.query(`SELECT * FROM sortgenre('${ category }')`);
+    res.status(200);
+    res.json(result.rows);
+  }
 }
 
 module.exports = { fetchGames };
