ad172c216b2dd4c46381b6b6adeefb32956bbfac
AndoPatrik <screadern@gmail.com>
2023-04-01 17:36:49 +0200

    UPDATE: fetchGames with pagination

--- API/src/controllers/gameController.js
+++ API/src/controllers/gameController.js
@@ -1,15 +1,22 @@
 const client = require("../config/databaseConnection.js");
+
 async function fetchGames (req, res) {
-  let { category, search } = req.query
+  let { category, search, page, limit, } = req.query
+
+  if (!(page && limit)) {
+    page = 1
+    limit = 20
+  }
+
 
   if (search) {
-    const result = await client.query(`SELECT * FROM search('${ search }')`);
+    const result = await client.query(`SELECT * FROM search('${ search }', ${ page }, ${ limit })`);
     res.status(200);
     res.json(result.rows);
   }
   else {
     if (!category) category = 'all'
-    const result = await client.query(`SELECT * FROM sortgenre('${ category }')`);
+    const result = await client.query(`SELECT * FROM sortgenre('${ category }', ${ page }, ${ limit })`);
     res.status(200);
     res.json(result.rows);
   }
