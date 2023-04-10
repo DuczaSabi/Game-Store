71c6352f87fa4893d128fd22767abb65e92a3374
AndoPatrik <screadern@gmail.com>
2023-04-10 18:10:29 +0200

    UPDATE: wrap endpoints in try catch

--- API/src/controllers/gameController.js
+++ API/src/controllers/gameController.js
@@ -1,109 +1,122 @@
 const client = require("../config/databaseConnection.js");
 
 async function fetchGames (req, res) {
-  let { category, search, page, limit } = req.query;
+  try {
+    let { category, search, page, limit } = req.query;
 
-  if (!(page && limit)) {
-    page = 1;
-    limit = 20;
-  }
+    if (!(page && limit)) {
+      page = 1;
+      limit = 20;
+    }
 
-  if (search) {
-    const result = await client.query(
-      `SELECT * FROM search('${ search }', ${ page }, ${ limit })`
-    );
-    const count = await client.query(`SELECT * FROM search_count('${ search }')`);
-    const returnObj = {
-      page: page,
-      limit: limit,
-      count: count.rows[0].search_count,
-      data: result.rows,
-    };
-    res.status(200);
-    res.json(returnObj);
-  } else {
-    if (!category) category = 0;
-    const result = await client.query(
-      `SELECT * FROM sortgenre('${ category }', ${ page }, ${ limit })`
-    );
-    const count = await client.query(
-      `SELECT * FROM sortgenre_count('${ category }')`
-    );
-    const returnObj = {
-      page: page,
-      limit: limit,
-      count: count.rows[0].sortgenre_count,
-      data: result.rows,
-    };
-    res.status(200);
-    res.json(returnObj);
-  }
+    if (search) {
+      const result = await client.query(
+        `SELECT * FROM search('${ search }', ${ page }, ${ limit })`
+      );
+      const count = await client.query(`SELECT * FROM search_count('${ search }')`);
+      const returnObj = {
+        page: page,
+        limit: limit,
+        count: count.rows[0].search_count,
+        data: result.rows,
+      };
+      res.status(200);
+      res.json(returnObj);
+    } else {
+      if (!category) category = 0;
+      const result = await client.query(
+        `SELECT * FROM sortgenre('${ category }', ${ page }, ${ limit })`
+      );
+      const count = await client.query(
+        `SELECT * FROM sortgenre_count('${ category }')`
+      );
+      const returnObj = {
+        page: page,
+        limit: limit,
+        count: count.rows[0].sortgenre_count,
+        data: result.rows,
+      };
+      res.status(200);
+      res.json(returnObj);
+    }
+  } catch (err) { console.log(err) }
 }
 
 async function modifyGame (req, res) {
-  let { Id, Title, Image, Genre, Link, Price } = req.body;
+  try {
+    let { Id, Title, Image, Genre, Link, Price } = req.body;
 
-  if (Id) {
-    const result = await client.query(
-      `SELECT updategame(${ Id }, '${ Title }', '${ Image }', '${ Genre }', '${ Link }', ${ Price })`
-    );
-    if (result) {
-      res.status(200);
-      res.send("Update successful!");
+    if (Id) {
+      const result = await client.query(
+        `SELECT updategame(${ Id }, '${ Title }', '${ Image }', '${ Genre }', '${ Link }', ${ Price })`
+      );
+      if (result) {
+        res.status(200);
+        res.send("Update successful!");
+      } else {
+        res.status(400);
+        res.send("Update failed");
+      }
     } else {
-      res.status(400);
-      res.send("Update failed");
+      res.status(404).send("Id required!");
     }
-  } else {
-    res.status(404).send("Id required!");
   }
+  catch (err) { console.log(err) }
 }
 
 async function addGame (req, res) {
-  let { Title, Image, Genre, ReleaseDate, Link, Price } = req.body;
+  try {
+    let { Title, Image, Genre, ReleaseDate, Link, Price } = req.body;
 
-  if (Title || Image || Genre || ReleaseDate || Link || Price) {
-    console.log("🚀 ~ addGame ~ Price:", Price);
-    console.log("🚀 ~ addGame ~ Link:", Link);
-    console.log("🚀 ~ addGame ~ ReleaseDate:", ReleaseDate);
-    console.log("🚀 ~ addGame ~ Genre:", Genre);
-    console.log("🚀 ~ addGame ~ Image:", Image);
-    console.log("🚀 ~ addGame ~ Title:", Title);
-    const title = Title;
+    if (Title || Image || Genre || ReleaseDate || Link || Price) {
+      console.log("🚀 ~ addGame ~ Price:", Price);
+      console.log("🚀 ~ addGame ~ Link:", Link);
+      console.log("🚀 ~ addGame ~ ReleaseDate:", ReleaseDate);
+      console.log("🚀 ~ addGame ~ Genre:", Genre);
+      console.log("🚀 ~ addGame ~ Image:", Image);
+      console.log("🚀 ~ addGame ~ Title:", Title);
+      const title = Title;
 
-    const result = await client.query(
-      `INSERT INTO public."Game"(
-        "Title", "Image", "Publisher", "ReleaseDate", "Genre", "Size", "Link", "Price")
-        VALUES ('${ Title }', '${ Image }', ' ', '${ ReleaseDate }', '${ Genre }',  0, '${ Link }', ${ Price });`
-    );
-    if (result) {
-      res.status(200);
-      res.send("Game added successfully!");
+      const result = await client.query(
+        `INSERT INTO public."Game"(
+          "Title", "Image", "Publisher", "ReleaseDate", "Genre", "Size", "Link", "Price")
+          VALUES ('${ Title }', '${ Image }', ' ', '${ ReleaseDate }', '${ Genre }',  0, '${ Link }', ${ Price });`
+      );
+      if (result) {
+        res.status(200);
+        res.send("Game added successfully!");
+      } else {
+        res.status(400);
+        res.send("Failed to add game!");
+      }
     } else {
-      res.status(400);
-      res.send("Failed to add game!");
+      res.status(404).send("All fields required!");
     }
-  } else {
-    res.status(404).send("All fields required!");
+  } catch (error) {
+    console.log(error)
   }
 }
 
 async function deleteGame (req, res) {
-  let Id = req.params.Id;
+  try {
+    let Id = req.params.Id;
 
-  if (Id) {
-    const result = await client.query(
-      `DELETE FROM "Game" WHERE "Id" = '${ Id }'`
-    );
-    if (result) {
-      res.status(200);
-      res.send("Delete successful!");
+    if (Id) {
+      const result = await client.query(
+        `DELETE FROM "Game" WHERE "Id" = '${ Id }'`
+      );
+      if (result) {
+        res.status(200);
+        res.send("Delete successful!");
+      } else {
+        res.status(400);
+        res.send("Delete failed");
+      }
     } else {
-      res.status(400);
-      res.send("Delete failed");
+      res.status(404).send("Id required!");
     }
-  } else {
-    res.status(404).send("Id required!");
+  } catch (error) {
+    console.log(error)
   }
 }
 
--- API/src/controllers/genreController.js
+++ API/src/controllers/genreController.js
@@ -1,9 +1,13 @@
 const client = require("../config/databaseConnection.js");
 
 async function getGenres (req, res) {
-  const result = await client.query('SELECT * FROM public."Genres"')
-  if (result) res.status(200).json(result.rows)
-  else res.status(404).send('Genres could not be fetched.')
+  try {
+    const result = await client.query('SELECT * FROM public."Genres"')
+    if (result) res.status(200).json(result.rows)
+    else res.status(404).send('Genres could not be fetched.')
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 module.exports = {
